1aad8f7fa0ce8f5e2b239e62ea348699194a32b9
Slaine W <slainew@gmail.com>
2024-01-11 16:48:55 +0000

    asd

--- index.js
+++ index.js
@@ -1,76 +1,90 @@
-const core = require('@actions/core');
-const axios = require('axios');
+const core = require("@actions/core");
+const axios = require("axios");
 
 async function main() {
+  try {
+    const variableSet = core.getInput("variableSetName");
+    const organizationName = core.getInput("organizationName");
+    const token = core.getInput("terraformToken");
+    const terraformHost = "app.terraform.io";
+    const variableValue = core.getInput("variableValue");
+    const variableName = core.getInput("variableName");
+    let variableHCL = core.getInput("variableHCL");
 
-    try{
-
-        const workSpaceName = core.getInput('workSpaceName');
-        const organizationName = core.getInput('organizationName');
-        const token = core.getInput('terraformToken');
-        const terraformHost = core.getInput('terraformHost');
-        const variableValue = core.getInput('variableValue');
-        const variableName = core.getInput('variableName');
-        let variableHCL = core.getInput('variableHCL');
-
-        if (variableHCL !== 'true'){
-            variableHCL = 'false';
-        }
-
-        const options = {
-            headers: {'Content-Type': 'application/vnd.api+json',
-                'Authorization': 'Bearer '+token
-            }
-        };
-
-        // Fetching WorkSpaceId
-        // TODO: Throw exception if workspace don't exist.
-        const terraformWorkSpaceEndpoint = "https://"+terraformHost+"/api/v2/organizations/"+organizationName+"/workspaces/"+workSpaceName;
-        const response = await axios.get(terraformWorkSpaceEndpoint,options);
-        const workSpaceId = response.data.data.id;
-        console.log("workSpaceId:"+workSpaceId)
-
-        // Fetch Terraform Variable ID
-        const terraformListVariablesEndpoint = "https://"+terraformHost+"/api/v2/vars/?filter[organization][name]="+organizationName+"&filter[workspace][name]="+workSpaceName;
-        const response2 = await axios.get(terraformListVariablesEndpoint,options);
-        const workspaceVariables = response2.data.data;
-        let variableId = null;
-        for (let variable of workspaceVariables) {
-            if(variable.attributes.key === variableName){
-                variableId = variable.id
-            }
-        }
-        // Check to make sure we found said variable
-        if (variableId == null) {
-            core.setFailed('Variable Could Not Be Found');
-        }
-        console.log("variableID:"+variableId)
-
-        // Update variable value we've found from missing step.
-        const terraformVariableUpdateEndpoint = "https://"+terraformHost+"/api/v2/vars/"+variableId;
-        let request = { data : {
-                id: variableId,
-                attributes: {
-                    "value" : variableValue,
-                    "hcl" : variableHCL
-                },
-                type: "vars",
-            }};
-        console.log("run request:" + JSON.stringify(request));
-
-        // Invoking Terraform Variable Patch API
-        axios.patch(terraformVariableUpdateEndpoint, request, options)
-            .then((response) => {
-                console.log("Update Variable Success:"+ JSON.stringify(response.data));
-                core.setOutput("variableId", response.data.data.id);
-            }, (error) => {
-                console.error("Update Variable Error:"+JSON.stringify(error.response.data));
-                core.setFailed(error.message);
-            });
-
-    } catch(error){
-        core.setFailed(error.message);
+    if (variableHCL !== "true") {
+      variableHCL = "false";
     }
+
+    const options = {
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    // Fetching WorkSpaceId
+    // TODO: Throw exception if workspace don't exist.
+    const terraformWorkSpaceEndpoint =
+      "https://" +
+      terraformHost +
+      "/api/v2/organizations/" +
+      organizationName +
+      "/varsets";
+    const response = await axios.get(terraformWorkSpaceEndpoint, options);
+    var variableSetId = response.data.find(
+      (item) => item.attributes.name === variableSet
+    )?.id;
+    console.log("variable set id:" + variableSetId);
+
+    if (variableSetId == null) {
+      core.setFailed("Variable Set Could Not Be Found");
+    }
+
+    // find variable id from variable name in variable set
+    const terraformVariableSetEndpoint =
+      "https://" + terraformHost + "/api/v2/varsets/" + variableSetId + "/vars";
+    const response1 = await axios.get(terraformVariableSetEndpoint, options);
+    var variableId = response1.data.data.find(
+      (item) => item.attributes.key === variableName
+    )?.id;
+
+    console.log("variable id:" + variableId);
+    if (variableId == null) { // if variable not found, create it
+        console.log("variable not found, creating it");
+        const terraformVariableSetEndpoint =
+            "https://" + terraformHost + "/api/v2/varsets/" + variableSetId + "/vars";
+        const response1 = await axios.post(terraformVariableSetEndpoint, {
+            data: {
+            type: "vars",
+            attributes: {
+                key: variableName,
+                value: variableValue,
+                hcl: variableHCL,
+                category: "terraform",
+            },
+            },
+        }, options);
+        console.log("variable created");
+    }else{
+        console.log("variable found, updating it");
+        const terraformVariableSetEndpoint =
+            "https://" + terraformHost + "/api/v2/varsets/" + variableSetId + "/vars/" + variableId;
+        const response1 = await axios.patch(terraformVariableSetEndpoint, {
+            data: {
+            type: "vars",
+            attributes: {
+                key: variableName,
+                value: variableValue,
+                hcl: variableHCL,
+                category: "terraform",
+            },
+            },
+        }, options);
+        console.log("variable updated");
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main();
